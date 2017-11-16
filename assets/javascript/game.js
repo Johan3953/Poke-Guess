bca088a652c8baed4770c885f69b78b393f10c1c
Johan3953 <JohanManuel3953@gmail.com>
2017-11-16 18:18:38 -0500

    W/L Recorded

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,7 @@
-var caught = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
-var seen = 0
-var x = 0
-var y = 0
-var win = 0
-var lose = 0
+var caught = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+var seen = 0;
+var win = 0;
+var lose = 0;
 
 $("#pokedex").text(caught);
 
@@ -13,15 +11,25 @@ $(".ball").on("click", function () {
 
     if (seen === caught) {
         alert("You caught them all! You're the very best!");
-        win  = x++
-        document.write("Battles Won: " + win);
-        window.location.reload();
+        win++;
+        console.log("Battles Won: " + win);
+        // window.location.reload();
+        startgame();
 
     } else if (seen >= caught) {
         alert("You can't catch more than " + caught + " You don't have enough badges.");
-        lose  = y++
-        conole.log("Battles Lost: " + lose);
-        window.location.reload();
-        
+        lose++;
+        console.log("Battles Lost: " + lose);
+        // window.location.reload();
+        startgame();
+
     }
 });
+
+function startgame() {
+    caught = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    seen = 0;
+    win = 0;
+    lose = 0;
+    $("#pokedex").text(caught);
+}
